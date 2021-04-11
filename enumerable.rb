4ca7d9ded86b7abc6e9122ee361e2f9783102c2a
joshuaivie <joshua@lifeonline.com.ng>
2021-04-11 19:41:58 +0100

    Add Ruby Linters and Github Workflow

--- enumerable.rb
+++ enumerable.rb
@@ -13,4 +13,19 @@ module Enumerable
       to_enum(:my_each)
     end
   end
+
+  def my_each_with_index
+    size = to_a.length
+    index = 0
+
+    if block_given?
+      until index == size
+        yield(to_a[index], index)
+        index += 1
+      end
+      self
+    else
+      to_enum(:my_each_with_index)
+    end
+  end
 end
